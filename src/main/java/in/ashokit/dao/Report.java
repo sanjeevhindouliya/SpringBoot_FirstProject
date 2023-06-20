da16db1aeaa0503e44892571596533945de690c4
sanju <sanjeevhindouliya@gmail.com>
2023-06-20 04:04:43 -0700

    springboot

---
+++ src/main/java/in/ashokit/dao/Report.java
@@ -0,0 +1,12 @@
+package in.ashokit.dao;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Report {
+	
+	public Report() {
+		System.out.println("report : constructor");
+	}
+
+}
